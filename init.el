0d7e9639e4986f66dbf47a6a5fdf42b5534d3810
nafiz1001 <nafiz.islam1001@gmail.com>
2022-12-16 21:04:38 -0500

    HEAD commit and single branch clone

--- init.el
+++ init.el
@@ -121,7 +121,7 @@
 
 ;; Straight
 
-(setq straight-vc-git-default-clone-depth 1)
+(setq straight-vc-git-default-clone-depth '(1 single-branch))
 (setq package-enable-at-startup nil)
 (setq straight-use-package-by-default t)
 
