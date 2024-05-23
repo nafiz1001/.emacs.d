e6f6496229d192fc577369335e6f7b8b9e43e55c
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-05-23 07:13:10 -0400

    2024-05-23

---
+++ init.el
@@ -0,0 +1,29 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(completion-styles '(basic partial-completion emacs22 substring))
+ '(completions-format 'one-column)
+ '(custom-enabled-themes '(modus-vivendi))
+ '(electric-pair-mode t)
+ '(global-display-line-numbers-mode t)
+ '(icomplete-mode t)
+ '(inhibit-startup-screen t)
+ '(mouse-wheel-progressive-speed nil)
+ '(package-selected-packages '(no-littering))
+ '(package-vc-selected-packages
+   '((no-littering :vc-backend Git :url "https://github.com/emacscollective/no-littering")))
+ '(proced-show-remote-processes t)
+ '(scroll-conservatively 101)
+ '(size-indication-mode t)
+ '(tab-bar-mode t)
+ '(tool-bar-mode nil)
+ '(visible-bell t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
