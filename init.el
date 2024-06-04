11c71982f5bc4b31eecf30d142ea07a9753e82e4
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-06-03 21:36:20 -0400

    2024-06-03
    - use-package

--- init.el
+++ init.el
@@ -1,48 +1,40 @@
 ;; -*- lexical-binding: t -*-
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(completion-styles '(basic partial-completion emacs22 substring))
- '(completions-format 'one-column)
- '(custom-enabled-themes '(modus-vivendi))
- '(electric-pair-mode t)
- '(global-display-line-numbers-mode t)
- '(icomplete-mode t)
- '(inhibit-startup-screen t)
- '(mouse-wheel-progressive-speed nil)
- '(org-directory "~/Documents/Org")
- '(package-selected-packages '(editorconfig markdown-mode nix-mode no-littering))
- '(package-vc-selected-packages
-   '((no-littering :vc-backend Git :url "https://github.com/emacscollective/no-littering")))
- '(proced-show-remote-processes t)
- '(scroll-conservatively 101)
- '(size-indication-mode t)
- '(tab-bar-mode t)
- '(tool-bar-mode nil)
- '(visible-bell t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(global-auto-revert-mode)
-(put 'dired-find-alternate-file 'disabled nil)
-;; (setq completion-ignore-case t) TODO: figure out how to make project-find-file case insensitive
-
-(package-install-selected-packages t)
-(package-vc-install-selected-packages)
+(unless (package-installed-p 'vc-use-package)
+  (package-vc-install "https://github.com/slotThe/vc-use-package"))
+(require 'vc-use-package)
 
 (use-package no-littering
+  :vc (:fetcher github :repo emacscollective/no-littering)
   :config
   (no-littering-theme-backups))
 
+(use-package emacs
+  :custom
+  (column-number-mode t)
+  (completion-styles '(basic partial-completion emacs22 substring))
+  (completions-format 'one-column)
+  (custom-enabled-themes '(modus-vivendi))
+  (electric-pair-mode t)
+  (global-display-line-numbers-mode t)
+  (icomplete-mode t)
+  (inhibit-startup-screen t)
+  (mouse-wheel-progressive-speed nil)
+  (proced-show-remote-processes t)
+  (scroll-conservatively 101)
+  (size-indication-mode t)
+  (tab-bar-mode t)
+  (tool-bar-mode nil)
+  (visible-bell t)
+  :config
+  (global-auto-revert-mode)
+  (put 'dired-find-alternate-file 'disabled nil)
+  ;; (setq completion-ignore-case t) TODO: figure out how to make project-find-file case insensitive
+  )
+
 (use-package org
+  :custom
+  (org-directory "~/Documents/Org")
   :config
   ;; TODO: make org-mode-hook initial functions not lambda
   (cl-pushnew 'visual-line-mode org-mode-hook))
@@ -59,5 +51,6 @@
 					("typescript-language-server" "--stdio"))))
 
 (use-package editorconfig
+  :ensure t
   :config
   (editorconfig-mode 1))
